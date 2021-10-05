f7a342bac43b03ef35eb594d4df767fd99016a63
shawnbae <tngks0315@naver.com>
2021-10-05 22:34:40 +0900

    20211005 update

--- Frontend/django/first_django/board/models.py
+++ Frontend/django/first_django/board/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# 아래와 같은 class 형식으로 model을 사용
+# class Board(models.Model):
+#   __class__
--- Frontend/django/first_django/board/views.py
+++ Frontend/django/first_django/board/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 아래와 같은 형식으로 request들을 처리함.
+#def test(request):
+#    pass
+#    pass
+
+#    return httpresponse
