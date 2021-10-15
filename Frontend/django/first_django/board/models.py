9d985f6efa21740f0977db9a73ecc892d5216424
shawnbae <tngks0315@naver.com>
2021-10-15 19:51:18 +0900

    20211015 update

--- Frontend/django/first_django/board/models.py
+++ Frontend/django/first_django/board/models.py
@@ -4,3 +4,15 @@ from django.db import models
 # 아래와 같은 class 형식으로 model을 사용
 # class Board(models.Model):
 #   __class__
+# 모델 만들기
+class Fcuser(models.Model):
+    username = models.CharField(max_length=64,
+                                verbose_name="사용자명")
+    password = models.CharField(max_length=64,
+                                verbose_name="비밀번호")
+    registered_dttm = models.DateTimeField(auto_now_add=True,
+                                verbose_name="등록시간")
+
+    # django Framework에 table 이름을 전달하는 역할
+    class Meta:
+        db_table = 'fastcampus_fcuser'                            
