aa809edd06b96a750f04de5e46b717b5c975d7f3
shawnbae <tngks0315@naver.com>
2021-10-23 15:38:49 +0900

    20211023 update

--- Others/Advanced_Python/class_2.py
+++ Others/Advanced_Python/class_2.py
@@ -51,13 +51,18 @@ class Student(object):
     def student_const(cls, id, first_name, last_name, email, grade, tuition, gpa):
         return cls(id, first_name, last_name, email, grade, tuition * cls.tuition_per, gpa)
 
-
-
-
-
-
-
-
+    # Static Method
+    # --> Class와 밀접한 일을 하는 Method에 대해, 
+    # self라는 인자도 받지 않고 cls라는 인자도 받지 않는
+    # inst라는 변수로 누구나 공통으로 사용 가능한 Method
+    @staticmethod
+    def is_scholarship_st(inst):
+        if inst._gpa >= 4.3:
+            return '{} is a scholarship recipient.'.format(inst._last_name)
+        return 'Sorry. Not a scholarship recipient.'
+    
+    def __str__(self):
+        return 'Student Info -> name: {} grade: {} email: {}'.format(self.full_name(), self._grade, self._email)
 
 # 학생 인스턴스    
 student_1 = Student(1, 'Kim', 'Sarang', 'Student1@naver.com', '1', 400, 3.5)
@@ -94,19 +99,30 @@ student_3 = Student.student_const(3, 'Park', 'Minji', 'Student3@gmail.com', '3',
 student_4 = Student.student_const(4, 'Cho', 'Sunghan', 'Student4@naver.com', '4', 600, 4.1)
 
 # 전체 정보
-print(student_3.detail_info())
-print(student_4.detail_info())
-print()
+#print(student_3.detail_info())
+#print(student_4.detail_info())
+#print()
 
 # 학생 학비 변경 확인
-print(student_3._tuition)
-print(student_4._tuition)
+#print(student_3._tuition)
+#print(student_4._tuition)
+#print()
+
+# 장학금 혜택 여부(스테이틱 메소드 미사용)
+def is_scholarship(inst):
+    if inst._gpa >= 4.3:
+        return '{} is a scholarship recipient.'.format(inst._last_name)
+    return 'Sorry. Not a scholarship recipient.'
+
+# 장학금 혜택 여부(스테이틱 메소드 사용)
+print('Static : ', Student.is_scholarship_st(student_1))
+print('Static : ', Student.is_scholarship_st(student_2))
+print('Static : ', Student.is_scholarship_st(student_3))
+print('Static : ', Student.is_scholarship_st(student_4))
 print()
 
-
-
-
-
-
-
-
+# 더불어, 각 self 객체들에 대해서도 staticmethod를 사용할 수 있다.
+print('Static : ', student_1.is_scholarship_st(student_1))
+print('Static : ', student_2.is_scholarship_st(student_2))
+print('Static : ', student_3.is_scholarship_st(student_3))
+print('Static : ', student_4.is_scholarship_st(student_4))
