6ca3fca114786c74d66e6eda607f0dab96e8b189
shawnbae <tngks0315@naver.com>
2021-10-24 14:13:35 +0900

    20211024 update

---
+++ Others/Advanced_Python/Advanced_List_Tuple.py
@@ -0,0 +1,3 @@
+# 컨테이너(Container : 서로다른 자료형[list, tuple, collections.deque], Flat : 한 개의 자료형[str,bytes,bytearray,array.array, memoryview])
+# 가변(list, bytearray, array.array, memoryview, deque) vs 불변(tuple, str, bytes)
+
