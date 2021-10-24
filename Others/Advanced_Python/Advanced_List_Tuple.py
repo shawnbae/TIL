d6e9f3c7a20d6f5b096ad6ee156ee6101235d567
shawnbae <tngks0315@naver.com>
2021-10-24 14:24:38 +0900

    20211024 update

--- Others/Advanced_Python/Advanced_List_Tuple.py
+++ Others/Advanced_Python/Advanced_List_Tuple.py
@@ -8,9 +8,14 @@ codes1 = []
 
 for s in chars:
     codes1.append(ord(s)) # Unicode값 반환
-
 print(codes1)
 
 # Comprehending Lists
 codes2 = [ord(s) for s in chars]
 print(codes2)
+
+codes3 = [ord(s) for s in chars if ord(s) > 40] # 조건부 list comprehension
+print(codes3)
+
+codes4 = list(filter(lambda x : x > 40, map(ord, chars))) # filter와 map을 활용한 방식
+print(codes4)
