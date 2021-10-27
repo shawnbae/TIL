7ad186733583f0671bae6427aa40274a1607b926
shawnbae <tngks0315@naver.com>
2021-10-27 14:16:06 +0900

    20211027 update

--- Others/Advanced_Python/Sequences_dictionary.py
+++ Others/Advanced_Python/Sequences_dictionary.py
@@ -11,3 +11,18 @@ t2 = (10, 20, [30, 40, 50])
 #print(hash(t2)) # 에러 발생 --> list는 가변적인 type이므로 hash값이 존재할 수 없음.
 
 # 지능형 딕셔너리 (Comprehending Dict)
+import csv
+
+# 외부 csv파일을 List of tuple형태로 만들기
+
+with open('./Others/Advanced_Python/resources/test1.csv', 'r', encoding='UTF-8') as f:
+    temp = csv.reader(f) # Generator
+    # header Skip
+    next(temp)
+    # 변환하기
+    NA_CODES = [tuple(x) for x in temp]
+
+#print(NA_CODES)
+
+n_code1 = {country: code for country, code in NA_CODES} # 각각을 key, value로 맵핑해줌
+print(n_code1)
