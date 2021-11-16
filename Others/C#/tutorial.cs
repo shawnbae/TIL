45d570fffb32cf388d9ae192ce447fcf7821731a
shawnbae <tngks0315@naver.com>
2021-11-16 16:34:28 +0900

    20211116 update

---
+++ Others/C#/tutorial.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Project명
+namespace Giraffe
+{
+   class Program
+   {
+        static void Main(string[] args) // Main is function!
+        {
+            Console.WriteLine("Hello World!");
+        }
+   }
+}
