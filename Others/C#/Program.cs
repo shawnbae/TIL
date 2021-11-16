c3456e357a98d79aaf9c4c1ead75fff9a9859701
shawnbae <tngks0315@naver.com>
2021-11-16 16:45:16 +0900

    20211116 update

---
+++ Others/C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Project명
+namespace Giraffe
+{
+   class Program
+   {
+        static void Main(string[] args) // Main is function!
+        {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+            Console.WriteLine("There once was a man naemd " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+        }
+   }
+}
