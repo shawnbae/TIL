2cdc2e74b52ec50c48a23a04e0040a30bba3fd83
soohan <tngks0315@naver.com>
2022-04-22 22:27:07 +0900

    20220422 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch02/src/Ex2_10.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch02/src/Ex2_10.java
@@ -1,4 +1,4 @@
-import java.util.Scanner; // Scanner�� ����ϱ� ���� �߰�
+import java.util.Scanner;
 
 class Ex2_10 {
 	public static void main(String[] args) {
@@ -12,3 +12,18 @@ class Ex2_10 {
 		System.out.printf("num=%d%n", num);
 	}
 }
+
+// import java.util.Scanner;
+
+// class Ex2_10 {
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+
+// System.out.print("두자리 정수를 하나 입력해주세요.>");
+// String input = scanner.nextLine();
+// int num = Integer.parseInt(input);
+
+// System.out.println("입력내용 :" + input);
+// System.out.printf("num=%d%n", num);
+// }
+// }
--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch02/src/Ex2_12.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch02/src/Ex2_12.java
@@ -2,7 +2,7 @@ class Ex2_12 {
 	public static void main(String args[]) {
 		String str = "3";
 
-		System.out.println(str.charAt(0) - '0');
+		System.out.println(str.charAt(0) - '0'); // 숫자를 문자로 변환 (숫자에 '0' 더하기)
 		System.out.println('3' - '0' + 1);
 		System.out.println(Integer.parseInt("3") + 1);
 		System.out.println("3" + 1);
