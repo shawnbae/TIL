d4a46300e9e2e34dc6dd936e9c91a12f4d447f6e
soohan <tngks0315@naver.com>
2022-05-14 23:43:25 +0900

    20220514 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch03/src/Ex3_1.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch03/src/Ex3_1.java
@@ -2,7 +2,7 @@ class Ex3_1 {
 	public static void main(String[] args) {
 		int x, y;
 
-		x = y = 3; // y�� 3�� ����� �Ŀ�, x�� 3�� ����ȴ�.
+		x = y = 3;
 		System.out.println("x=" + x);
 		System.out.println("y=" + y);
 	}
