589fe842ee6e2c56a3678d6bf5d8e3d103e5754c
soohan <tngks0315@naver.com>
2022-05-15 00:12:08 +0900

    20220515 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch03/src/Ex3_1.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch03/src/Ex3_1.java
@@ -2,7 +2,7 @@ class Ex3_1 {
 	public static void main(String[] args) {
 		int x, y;
 
-		x = y = 3;
+		x = y = 3; // y에 3이 저장된 후에, x에 3이 저장된다.
 		System.out.println("x=" + x);
 		System.out.println("y=" + y);
 	}
