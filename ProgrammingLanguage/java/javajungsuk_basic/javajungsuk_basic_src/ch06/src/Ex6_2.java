381adef3e803a957f556f31d4c0843e09e63fe55
soohan <tngks0315@naver.com>
2022-05-24 19:43:34 +0900

    20220524 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch06/src/Ex6_2.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch06/src/Ex6_2.java
@@ -1,14 +1,14 @@
 class Ex6_2 {
 	public static void main(String args[]) {
-		Tv t1 = new Tv();  // Tv t1; t1 = new Tv();¸¦ ÇÑ ¹®ÀåÀ¸·Î °¡´É
+		Tv t1 = new Tv(); // Tv t1; t1 = new Tv();ë¥¼ í•œ ë¬¸ì¥ìœ¼ë¡œ ê°€ëŠ¥
 		Tv t2 = new Tv();
-		System.out.println("t1ÀÇ channel°ªÀº " + t1.channel + "ÀÔ´Ï´Ù.");
-		System.out.println("t2ÀÇ channel°ªÀº " + t2.channel + "ÀÔ´Ï´Ù.");
+		System.out.println("t1ì˜ channelê°’ì€ " + t1.channel + "ì…ë‹ˆë‹¤.");
+		System.out.println("t2ì˜ channelê°’ì€ " + t2.channel + "ì…ë‹ˆë‹¤.");
 
-		t1.channel = 7;    // channel °ªÀ» 7À¸·Î ÇÑ´Ù.
-		System.out.println("t1ÀÇ channel°ªÀ» 7·Î º¯°æÇÏ¿´½À´Ï´Ù.");
+		t1.channel = 7; // channel ê°’ì„ 7ìœ¼ë¡œ í•œë‹¤.
+		System.out.println("t1ì˜ channelê°’ì„ 7ë¡œ ë³€ê²½í•˜ì˜€ìŠµë‹ˆë‹¤.");
 
-		System.out.println("t1ÀÇ channel°ªÀº " + t1.channel + "ÀÔ´Ï´Ù.");
-		System.out.println("t2ÀÇ channel°ªÀº " + t2.channel + "ÀÔ´Ï´Ù.");
+		System.out.println("t1ì˜ channelê°’ì€ " + t1.channel + "ì…ë‹ˆë‹¤.");
+		System.out.println("t2ì˜ channelê°’ì€ " + t2.channel + "ì…ë‹ˆë‹¤.");
 	}
 }
