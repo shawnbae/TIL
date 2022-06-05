beacf358f06daad0bb98d1085c463d89ac35d8b9
soohan <tngks0315@naver.com>
2022-06-05 12:18:54 +0900

    20220605 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch06/src/Ex6_10.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch06/src/Ex6_10.java
@@ -1,43 +1,43 @@
 class Ex6_10 {
 	public static void main(String args[]) {
 		MyMath3 mm = new MyMath3();
-		System.out.println("mm.add(3, 3) °á°ú:"    + mm.add(3,3));
-		System.out.println("mm.add(3L, 3) °á°ú: "  + mm.add(3L,3));
-		System.out.println("mm.add(3, 3L) °á°ú: "  + mm.add(3,3L));
-		System.out.println("mm.add(3L, 3L) °á°ú: " + mm.add(3L,3L));
+		System.out.println("mm.add(3, 3) ê²°ê³¼:" + mm.add(3, 3));
+		System.out.println("mm.add(3L, 3) ê²°ê³¼: " + mm.add(3L, 3));
+		System.out.println("mm.add(3, 3L) ê²°ê³¼: " + mm.add(3, 3L));
+		System.out.println("mm.add(3L, 3L) ê²°ê³¼: " + mm.add(3L, 3L));
 
-		int[] a = {100, 200, 300};
-		System.out.println("mm.add(a) °á°ú: " + mm.add(a));
-   }
+		int[] a = { 100, 200, 300 };
+		System.out.println("mm.add(a) ê²°ê³¼: " + mm.add(a));
+	}
 }
 
 class MyMath3 {
 	int add(int a, int b) {
 		System.out.print("int add(int a, int b) - ");
-		return a+b;
+		return a + b;
 	}
-	
+
 	long add(int a, long b) {
 		System.out.print("long add(int a, long b) - ");
-		return a+b;
+		return a + b;
 	}
-	
+
 	long add(long a, int b) {
 		System.out.print("long add(long a, int b) - ");
-		return a+b;
+		return a + b;
 	}
 
 	long add(long a, long b) {
 		System.out.print("long add(long a, long b) - ");
-		return a+b;
+		return a + b;
 	}
 
-	int add(int[] a) {		// ¹è¿­ÀÇ ¸ğµç ¿ä¼ÒÀÇ ÇÕÀ» °á°ú·Î µ¹·ÁÁØ´Ù.
+	int add(int[] a) { // ë°°ì—´ì˜ ëª¨ë“  ìš”ì†Œì˜ í•©ì„ ê²°ê³¼ë¡œ ëŒë ¤ì¤€ë‹¤.
 		System.out.print("int add(int[] a) - ");
 		int result = 0;
-		for(int i=0; i < a.length;i++) 
+		for (int i = 0; i < a.length; i++)
 			result += a[i];
-		
+
 		return result;
 	}
 }
