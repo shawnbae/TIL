4f7f798498a82df5e91cd352da164cbdff5cdf56
soohan <tngks0315@naver.com>
2022-06-07 20:48:31 +0900

    20220607 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch07/src/Ex7_6.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch07/src/Ex7_6.java
@@ -2,7 +2,7 @@ import static java.lang.System.out;
 import static java.lang.Math.*;
 
 class Ex7_6 {
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		// System.out.println(Math.random());
 		out.println(random());
 
