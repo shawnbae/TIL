1611ce4dbeceeb7677e181b55289ec201c6663dc
soohan <tngks0315@naver.com>
2022-06-07 20:44:13 +0900

    20220607 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch07/src/Ex7_3.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch07/src/Ex7_3.java
@@ -5,12 +5,14 @@ class Ex7_3 {
 	}
 }
 
-class Parent2 { int x=10; }
+class Parent2 {
+	int x = 10;
+}
 
 class Child2 extends Parent2 {
 	void method() {
 		System.out.println("x=" + x);
 		System.out.println("this.x=" + this.x);
-		System.out.println("super.x="+ super.x);
+		System.out.println("super.x=" + super.x);
 	}
 }
