094be643ded012cb292baa040b592f0dc73a8e58
soohan <tngks0315@naver.com>
2022-07-13 15:57:50 +0900

    20220713 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch06/src/practice.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch06/src/practice.java
@@ -1,27 +1,14 @@
-public class practice {
+class practice {
     public static void main(String args[]) {
-        Tv t;
-        t = new Tv();
-        t.channel = 7;
-        t.channelDown();
-        System.out.println("현재 채널은 " + t.channel + " 입니다.");
-    }
-}
+        Tv t1 = new Tv();
+        Tv t2 = new Tv();
+        System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
+        System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
 
-class Tv {
-    String color;
-    boolean power;
-    int channel;
+        t1.channel = 7;
+        System.out.println("t1의 channel값을 7로 변경하였습니다.");
 
-    void power() {
-        power = !power;
-    }
-
-    void channelUp() {
-        ++channel;
-    }
-
-    void channelDown() {
-        --channel;
+        System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
+        System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
     }
 }
