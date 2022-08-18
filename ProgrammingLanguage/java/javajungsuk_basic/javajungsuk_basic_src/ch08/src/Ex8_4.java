fee8e316ec01b459b2b72ea8a73b3efd1debb125
soohan <tngks0315@naver.com>
2022-08-18 14:24:54 +0900

    20220818 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch08/src/Ex8_4.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch08/src/Ex8_4.java
@@ -1,18 +1,18 @@
 class Ex8_4 {
 	public static void main(String args[]) {
-		System.out.println(1);			
+		System.out.println(1);
 		System.out.println(2);
 		try {
 			System.out.println(3);
-			System.out.println(0/0);
-			System.out.println(4); 	// ½ÇÇàµÇÁö ¾Ê´Â´Ù.
-		} catch (ArithmeticException ae)	{
-			if (ae instanceof ArithmeticException) 
-				System.out.println("true");	
+			System.out.println(0 / 0);
+			System.out.println(4); // ì‹¤í–‰ë˜ì§€ ì•ŠëŠ”ë‹¤.
+		} catch (ArithmeticException ae) {
+			if (ae instanceof ArithmeticException)
+				System.out.println("true");
 			System.out.println("ArithmeticException");
-		} catch (Exception e){
+		} catch (Exception e) {
 			System.out.println("Exception");
-		}	// try-catchÀÇ ³¡
+		} // try-catchì˜ ë
 		System.out.println(6);
-	}	// main¸Ş¼­µåÀÇ ³¡
+	} // mainë©”ì„œë“œì˜ ë
 }
