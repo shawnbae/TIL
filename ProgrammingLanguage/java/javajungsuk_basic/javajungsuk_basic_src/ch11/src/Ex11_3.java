1a617e36aba6a105dc7faaf1363fd392e0b93d85
soohan <tngks0315@naver.com>
2022-08-22 18:07:59 +0900

    20220822 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch11/src/Ex11_3.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch11/src/Ex11_3.java
@@ -25,12 +25,12 @@ public class Ex11_3 {
 			}
 
 			if (st.isEmpty()) {
-				System.out.println("°ıÈ£°¡ ÀÏÄ¡ÇÕ´Ï´Ù.");
+				System.out.println("ê´„í˜¸ê°€ ì¼ì¹˜í•©ë‹ˆë‹¤.");
 			} else {
-				System.out.println("°ıÈ£°¡ ÀÏÄ¡ÇÏÁö ¾Ê½À´Ï´Ù.");
+				System.out.println("ê´„í˜¸ê°€ ì¼ì¹˜í•˜ì§€ ì•ŠìŠµë‹ˆë‹¤.");
 			}
 		} catch (EmptyStackException e) {
-			System.out.println("°ıÈ£°¡ ÀÏÄ¡ÇÏÁö ¾Ê½À´Ï´Ù.");
+			System.out.println("ê´„í˜¸ê°€ ì¼ì¹˜í•˜ì§€ ì•ŠìŠµë‹ˆë‹¤.");
 		} // try
 	}
 }
