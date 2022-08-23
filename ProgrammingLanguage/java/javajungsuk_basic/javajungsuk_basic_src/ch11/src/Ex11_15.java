ddc3f2061d248e9051907e943a6c607d84d7e014
soohan <tngks0315@naver.com>
2022-08-23 14:12:54 +0900

    20220823 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch11/src/Ex11_15.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch11/src/Ex11_15.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Ex11_15 {
 	public static void main(String[] args) {
 		TreeSet set = new TreeSet();
-		int[] score = {80, 95, 50, 35, 45, 65, 10, 100};
+		int[] score = { 80, 95, 50, 35, 45, 65, 10, 100 };
 
-		for(int i=0; i < score.length; i++)
+		for (int i = 0; i < score.length; i++)
 			set.add(new Integer(score[i]));
 
-		System.out.println("50º¸´Ù ÀÛÀº °ª :" + set.headSet(new Integer(50)));
-		System.out.println("50º¸´Ù Å« °ª :"  + set.tailSet(new Integer(50)));
+		System.out.println("50ë³´ë‹¤ ìž‘ì€ ê°’ :" + set.headSet(new Integer(50)));
+		System.out.println("50ë³´ë‹¤ í° ê°’ :" + set.tailSet(new Integer(50)));
 	}
 }
---
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch11/src/practice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Ex11_14 {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet();
+
+        String from = "b";
+        String to = "d";
+
+        set.add()
+    }
+}
