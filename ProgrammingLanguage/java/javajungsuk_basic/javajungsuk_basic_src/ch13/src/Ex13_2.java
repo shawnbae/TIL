e6d4eeb3a67591c2f77e60c29fbc0a86615c878b
soohan <tngks0315@naver.com>
2022-08-29 17:49:53 +0900

    20220829 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch13/src/Ex13_2.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch13/src/Ex13_2.java
@@ -2,14 +2,14 @@ class Ex13_2 {
 	public static void main(String args[]) {
 		long startTime = System.currentTimeMillis();
 
-		for(int i=0; i < 300; i++)
-			System.out.printf("%s", new String("-"));		
+		for (int i = 0; i < 300; i++)
+			System.out.printf("%s", new String("-"));
 
-		System.out.print("�ҿ�ð�1:" +(System.currentTimeMillis()- startTime)); 
+		System.out.print("소요시간1:" + (System.currentTimeMillis() - startTime));
 
-		for(int i=0; i < 300; i++) 
-			System.out.printf("%s", new String("|"));		
+		for (int i = 0; i < 300; i++)
+			System.out.printf("%s", new String("|"));
 
- 		System.out.print("�ҿ�ð�2:"+(System.currentTimeMillis() - startTime));
+		System.out.print("소요시간2:" + (System.currentTimeMillis() - startTime));
 	}
 }
