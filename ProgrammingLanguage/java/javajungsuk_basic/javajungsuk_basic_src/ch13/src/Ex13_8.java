cdea5fb8afe92f379aee85bfb0014acf549248f4
soohan <tngks0315@naver.com>
2022-09-07 19:16:48 +0900

    20220907 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch13/src/Ex13_8.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch13/src/Ex13_8.java
@@ -2,26 +2,30 @@ class Ex13_8 {
 	public static void main(String args[]) {
 		ThreadEx8_1 th1 = new ThreadEx8_1();
 		ThreadEx8_2 th2 = new ThreadEx8_2();
-		th1.start(); th2.start();
+		th1.start();
+		th2.start();
 
 		try {
-			th1.sleep(2000);	
-		} catch(InterruptedException e) {}
+			th1.sleep(2000);
+		} catch (InterruptedException e) {
+		}
 
-		System.out.print("<<main Á¾·á>>");
+		System.out.print("<<main ì¢…ë£Œ>>");
 	} // main
 }
 
 class ThreadEx8_1 extends Thread {
 	public void run() {
-		for(int i=0; i < 300; i++) System.out.print("-");
-		System.out.print("<<th1 Á¾·á>>");
+		for (int i = 0; i < 300; i++)
+			System.out.print("-");
+		System.out.print("<<th1 ì¢…ë£Œ>>");
 	} // run()
 }
 
 class ThreadEx8_2 extends Thread {
 	public void run() {
-		for(int i=0; i < 300; i++) System.out.print("|");
-		System.out.print("<<th2 Á¾·á>>");
+		for (int i = 0; i < 300; i++)
+			System.out.print("|");
+		System.out.print("<<th2 ì¢…ë£Œ>>");
 	} // run()
 }
