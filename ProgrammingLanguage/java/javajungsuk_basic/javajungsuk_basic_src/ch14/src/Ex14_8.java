ab35a9986224dffc5dd102809bd4ceb36773f21d
soohan <tngks0315@naver.com>
2022-10-12 19:05:27 +0900

    20221012 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch14/src/Ex14_8.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch14/src/Ex14_8.java
@@ -2,35 +2,35 @@ import java.util.*;
 
 class Ex14_8 {
 	public static void main(String[] args) {
-		Optional<String>  optStr = Optional.of("abcde");
+		Optional<String> optStr = Optional.of("abcde");
 		Optional<Integer> optInt = optStr.map(String::length);
-		System.out.println("optStr="+optStr.get());
-		System.out.println("optInt="+optInt.get());
+		System.out.println("optStr=" + optStr.get());
+		System.out.println("optInt=" + optInt.get());
 
 		int result1 = Optional.of("123")
-						    .filter(x->x.length() >0)
-						    .map(Integer::parseInt).get();
+				.filter(x -> x.length() > 0)
+				.map(Integer::parseInt).get();
 
 		int result2 = Optional.of("")
-						    .filter(x->x.length() >0)
-						    .map(Integer::parseInt).orElse(-1);
+				.filter(x -> x.length() > 0)
+				.map(Integer::parseInt).orElse(-1);
 
-		System.out.println("result1="+result1);
-		System.out.println("result2="+result2);
+		System.out.println("result1=" + result1);
+		System.out.println("result2=" + result2);
 
 		Optional.of("456").map(Integer::parseInt)
-					      .ifPresent(x->System.out.printf("result3=%d%n",x));
+				.ifPresent(x -> System.out.printf("result3=%d%n", x));
 
-		OptionalInt optInt1  = OptionalInt.of(0);   // 0¿ª ¿˙¿Â
-		OptionalInt optInt2  = OptionalInt.empty(); // ∫Û ∞¥√º∏¶ ª˝º∫
+		OptionalInt optInt1 = OptionalInt.of(0); // 0ÏùÑ Ï†ÄÏû•
+		OptionalInt optInt2 = OptionalInt.empty(); // Îπà Í∞ùÏ≤¥Î•º ÏÉùÏÑ±
 
-		System.out.println(optInt1.isPresent());   // true
-		System.out.println(optInt2.isPresent());   // false
+		System.out.println(optInt1.isPresent()); // true
+		System.out.println(optInt2.isPresent()); // false
 
-		System.out.println(optInt1.getAsInt());  // 0
-//		System.out.println(optInt2.getAsInt());  // NoSuchElementException
-		System.out.println("optInt1="+optInt1);
-		System.out.println("optInt2="+optInt2);
-	  	System.out.println("optInt1.equals(optInt2)?"+optInt1.equals(optInt2));
+		System.out.println(optInt1.getAsInt()); // 0
+		// System.out.println(optInt2.getAsInt()); // NoSuchElementException
+		System.out.println("optInt1=" + optInt1);
+		System.out.println("optInt2=" + optInt2);
+		System.out.println("optInt1.equals(optInt2)?" + optInt1.equals(optInt2));
 	}
 }
