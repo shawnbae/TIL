b8a3d7f469c0fa6cef77b7303569940cf3f8e856
soohan <tngks0315@naver.com>
2022-12-01 15:33:13 +0900

    20221201 update

--- ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch14/src/practice.java
+++ ProgrammingLanguage/java/javajungsuk_basic/javajungsuk_basic_src/ch14/src/practice.java
@@ -1,13 +1,20 @@
 import java.util.*;
-import java.util.stream.*;
 
-class practice {
+class Ex14_8 {
     public static void main(String[] args) {
-        Stream<String[]> Streamtest = Stream.of(
-                new String[] { "abc", "def", "jkl" },
-                new String[] { "abc", "def", "jkl" });
+        Optional<String> optStr = Optional.of("abcde");
+        Optional<Integer> optInt = optStr.map(String::length);
+        System.out.println("optStr=" + optStr.get());
+        System.out.println("optInt=" + optStr.get());
 
-        // Streamtest.map(System.out::println);
-        Streamtest.flatMap(System.out::println);
+        int result1 = Optional.of("123")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt).get();
+
+        int result2 = Optional.of("")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt).orElse(-1);
+        
+        System.out.println();
     }
 }
