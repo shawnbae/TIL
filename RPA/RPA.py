1013f4e2272b90f194dec6d5e7a957ae5369d760
soohan <tngks0315@naver.com>
2023-05-30 17:33:26 +0900

    20230530 update

--- RPA/RPA.py
+++ RPA/RPA.py
@@ -5,3 +5,14 @@ import warnings
 import pythoncom
 import win32com.client
 import subprocess
+
+# 시간 처리
+from datetime import datetime, timedelta
+
+# Pandas
+import pandas as pd
+
+# 정의 class 사용하기
+import config
+
+warnings.filterwarnings('ignore')
